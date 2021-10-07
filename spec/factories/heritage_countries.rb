2a8d22d2f6a0eaafa6f06cae2ea7f8be14247ca2
komagenn <89065599+komagenn@users.noreply.github.com>
2021-10-07 21:20:48 +0900

    不要ファイルの削除

--- spec/factories/heritage_countries.rb
+++
@@ -1,9 +0,0 @@
-# FactoryBot.define do
-#   factory :heritage_country do
-#     name       {'ロス・グラシアス'}
-#     register_id  {'4'}
-#     country_id   {'4'}
-#     explain      {'あああ'}
-#    userこっちが本物
-#   end
-# end
